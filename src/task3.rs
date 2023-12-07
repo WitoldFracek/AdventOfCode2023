047dbca5c62c4811b77b9acc71895f61764d1a60
Witold Frącek <witek.fracek@gmail.com>
2023-12-07 08:42:58 +0100

    3 not working

--- src/task3.rs
+++ src/task3.rs
@@ -1,17 +1,37 @@
 use crate::utils::read_lines;
 
 pub fn solve() {
-    let lines = read_lines("./data/data3.txt");
+    let lines = read_lines("./data/data3_debug.txt");
     let schema = lines.iter()
         .map(|line| line.chars().collect::<Vec<char>>())
         .collect::<Vec<Vec<char>>>();
-    let number_indices = schema.iter()
-        .map(find_numbers)
-        .collect::<Vec<Vec<(usize, usize)>>>();
 
-    let input = String::from("123..^.23..").chars().collect::<Vec<char>>();
-    let res = find_numbers(&input);
-    println!("{res:?}");
+    // println!("{} {}", schema.len(), schema[0].len());
+
+    let res = schema.iter()
+        .map(find_numbers)
+        .enumerate()
+        .flat_map(|(i, v)| v.iter().map(|&x| (i, x)).collect::<Vec<_>>())
+        .filter(|&(i, range)| is_valid(range, i, &schema))
+        .collect::<Vec<(usize, (usize, usize))>>();
+    for elem in res {
+        println!("{elem:?}");
+    }
+
+    // let res = schema.iter()
+    //     .map(find_numbers)
+    //     .enumerate()
+    //     .flat_map(|(i, v)| v.iter().map(|&x| (i, x)).collect::<Vec<_>>())
+    //     .filter(|&(i, range)| is_valid(range, i, &schema))
+    //     .map(|(i, range)| number_from_range(range, i, &schema))
+    //     .sum::<i32>();
+    // println!("{res}");
+
+
+    // let input = String::from("..123..^.23..").chars().collect::<Vec<char>>();
+    // let res = find_numbers(&input);
+    // let res = get_bounding_box(res[0], 1, 3, 13);
+    // println!("{res}");
 }
 
 fn find_numbers(line: &Vec<char>) -> Vec<(usize, usize)> {
@@ -45,22 +65,37 @@ fn get_bounding_box(
     let line_no = line_no as i32;
     let schema_lines_count = schema_lines_count as i32;
     let schema_chars_count = schema_chars_count as i32;
-
-    let mut ret = Vec::new();
     let(start, stop) = (word.0 as i32, word.1 as i32);
 
-    let mut sx = start - 1;
-    let mut sy = line_no - 1;
-    let mut ex = stop + 1;
-    let mut ey = line_no + 1;
+    let mut sx = i32::max(start - 1, 0);
+    let mut sy = i32::max(line_no - 1, 0);
+    let mut ex = i32::min(stop + 1, schema_chars_count - 1);
+    let mut ey = i32::min(line_no + 1, schema_lines_count - 1);
 
-    if start == 0 {
-        sx = 0;
+    let mut ret = Vec::new();
+    for x in sx..=ex {
+        for y in sy..=ey {
+            if y == line_no {
+                if x < start || x > stop {
+                    ret.push((y as usize, x as usize))
+                }
+            } else {
+                ret.push((y as usize, x as usize))
+            }
+        }
     }
-    if stop + 1 == schema_chars_count {
-        ex = stop
-    }
-
-
+    // println!("{ret:?}");
     ret
+}
+
+fn is_valid(number_bounds: (usize, usize), line_no: usize, schema: &Vec<Vec<char>>) -> bool {
+    let bb = get_bounding_box(number_bounds, line_no, schema.len(), schema[0].len());
+    bb.iter()
+        .map(|&(x, y)|schema[x][y] != '.' && !schema[x][y].is_numeric())
+        .any(|x| x)
+}
+
+fn number_from_range(range: (usize, usize), line_no: usize, schema: &Vec<Vec<char>>) -> i32 {
+    let (start, stop) = range;
+    String::from_iter(schema[line_no][start..=stop].iter()).parse::<i32>().unwrap()
 }
